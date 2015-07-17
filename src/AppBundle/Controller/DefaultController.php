3f9efc345c3972e1466d0db77c8eb45d572199e0
Ivanis Kouamé <ivanis.kouame@gmail.com>
2015-07-18 00:46:29 +0200

    Integrate frontend with Symfony

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,32 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/privacy", name="privacy")
+     */
+    public function privacyAction()
+    {
+        return $this->render('default/privacy.html.twig');
+    }
+
+    /**
+     * @Route("/terms", name="terms")
+     */
+    public function termsAction()
+    {
+        return $this->render('default/terms.html.twig');
     }
 }
